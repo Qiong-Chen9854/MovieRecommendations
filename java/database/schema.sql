4918434eb5693df2a21b8132e53a93c2bed3eb90
Qiong Chen <chenqiong.chen@gmail.com>
2024-08-01 13:51:38 -0400

    create tables - movie, person, movie_person

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,31 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+
+-- create tables
+
+DROP TABLE IF EXISTS movie_person, person, movie ;
+
+CREATE TABLE movie(
+	movie_id serial PRIMARY KEY,
+	title varchar(225) NOT NULL,
+	release_date date,
+	description text
+);
+
+CREATE TABLE person(
+	person_id serial PRIMARY KEY,
+	first_name varchar(80),
+	last_name varchar(80),
+	role varchar(50)
+);
+
+CREATE TABLE movie_person(
+	movie_id int,
+	person_id int,
+	PRIMARY KEY (movie_id,person_id),
+	FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+	FOREIGN KEY (person_id) REFERENCES person(person_id)
+)
+
 COMMIT TRANSACTION;
